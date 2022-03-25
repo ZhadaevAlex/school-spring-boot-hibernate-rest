144bed4ec82b94fb98c262b08397f61ccd810be1
Alex Zhadaev <zhadaevs@yandex.ru>
2022-03-25 10:52:54 +0300

    Изменен тип integer на serial

--- createTables.sql
+++ createTables.sql
@@ -1,24 +1,24 @@
-drop table if exists school.groups;
-
 drop table if exists school.students;
+
+drop table if exists school.groups;
 
 drop table if exists school.courses;
 
 create table if not exists school.groups (
-    group_id integer primary key,
+    group_id serial primary key,
     group_name varchar(255)
 );
 
 create table if not exists school.students (
     student_id integer primary key,
-    group_id integer,
+    group_id serial,
     first_name varchar(255),
     last_name varchar(255),
     foreign key (group_id) references school.groups(group_id)
 );
 
 create table if not exists school.courses (
-    course_id integer,
+    course_id serial,
     course_name varchar(255),
     course_description text
 );
