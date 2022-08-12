f9ed093c0c5bae8127e08ad2082241b5a6bcb906
Alex Zhadaev <zhadaevs@yandex.ru>
2022-08-12 18:52:30 +0300

    Hibernate init commit

--- src/main/java/ru/zhadaev/dao/entities/Group.java
+++ src/main/java/ru/zhadaev/dao/entities/Group.java
@@ -2,8 +2,19 @@ package ru.zhadaev.dao.entities;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "school", name = "groups")
 @Data
 public class Group {
+    @Id
+    @Column(name = "group_id")
     private Integer id;
+
+    @Column(name = "group_name")
     private String name;
 }
