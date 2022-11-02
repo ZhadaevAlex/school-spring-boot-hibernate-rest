303b34e362f6801302a50b22182d3a568012831d
Alex Zhadaev <zhadaevs@yandex.ru>
2022-11-02 13:36:37 +0300

    Added springdoc-openapi-ui Added annotation for Group

--- src/main/java/ru/zhadaev/controllers/GroupController.java
+++ src/main/java/ru/zhadaev/controllers/GroupController.java
@@ -1,5 +1,12 @@
 package ru.zhadaev.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.zhadaev.dao.entities.Group;
@@ -18,12 +25,35 @@ public class GroupController {
     private final SchoolManager schoolManager;
     private final GroupMapper groupMapper;
 
+    @Operation(
+            summary = "Get all groups",
+            description = "This method allows you to get a list of all school groups")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = {@Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(
+                            schema = @Schema(
+                                    implementation = GroupDto.class)))})})
     @GetMapping()
     public List<GroupDto> findAll() {
         List<Group> groups = groupService.findAll();
         return groupMapper.groupsToGroupsDto(groups);
     }
 
+    @Operation(summary = "Add a new group",
+            description = "This method adds a new group to the school")
+    @RequestBody(
+            description = "You must specify the group parameters",
+            required = true)
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = {@Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = GroupDto.class))})})
     @PostMapping()
     public GroupDto save(@RequestBody GroupDto groupDto) {
         Group group = groupMapper.groupDtoToGroup(groupDto);
@@ -33,29 +63,72 @@ public class GroupController {
         return savedDto;
     }
 
+    @Operation(summary = "Get a group by id",
+            description = "This method allows you to get a school group by its id")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = {@Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = GroupDto.class))})})
     @GetMapping("/{id}")
     public GroupDto findById(@PathVariable("id") Integer id) {
         Group group = groupService.findById(id);
         return groupMapper.groupToGroupDto(group);
     }
 
+    @Operation(summary = "Delete a group",
+            description = "This method removes the group with the specified id")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation")})
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") Integer id) {
         groupService.deleteById(id);
     }
 
+    @Operation(summary = "Delete all groups",
+            description = "This resource represents an all groups in the system")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation")})
     @DeleteMapping()
     public void deleteAll() {
         groupService.deleteAll();
     }
 
+    @Operation(summary = "Edit a group",
+            description = "This method updates the group with the specified id. The values of the group properties are replaced with the values from the request body. Unspecified values are replaced with default values")
+    @RequestBody(
+            description = "You must specify the group parameters",
+            required = true)
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = {@Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = GroupDto.class))})})
     @PutMapping("/{id}")
     public GroupDto replace(@RequestBody GroupDto groupDto, @PathVariable("id") Integer id) {
         Group group = groupMapper.groupDtoToGroup(groupDto);
         Group replaced = groupService.update(group, id);
         return groupMapper.groupToGroupDto(replaced);
     }
-    
+
+    @Operation(summary = "Edit a group",
+            description = "This method updates the group with the specified id. The values of the group properties are replaced with the values from the request body. Unspecified values are not changed")
+    @RequestBody(
+            description = "You must specify the group parameters",
+            required = true)
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = {@Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = GroupDto.class))})})
     @PatchMapping("/{id}")
     public GroupDto update(@RequestBody GroupDto groupDto, @PathVariable("id") Integer id) {
         Group group = groupService.findById(id);
@@ -64,6 +137,16 @@ public class GroupController {
         return groupMapper.groupToGroupDto(updated);
     }
 
+    @Operation(summary = "Get filtered groups",
+            description = "This method returns a groups with less or equal student count")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = {@Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(
+                            schema = @Schema(
+                                    implementation = GroupDto.class)))})})
     @GetMapping("/filter")
     public List<GroupDto> findAllFilter(@RequestParam("numberStudents") Integer numberStudents) {
         List<Group> groups = schoolManager.findGroupsByNumberStudents(numberStudents);
